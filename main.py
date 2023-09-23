5577c31df0d7c3be4de19f17e468d71777a4527c
lixyleon <lixyleon2000gmail.com>
2023-09-23 12:27:56 -0400

    fix: modify file main.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 import time
-from http.server import HTTPServer
+from http.server import HTTPServer, SimpleHTTPRequestHandler, test
 from server import Server
 
 HOST_NAME = 'localhost'
 PORT_NUMBER = 8000
 
+class CORSRequestHandler (SimpleHTTPRequestHandler):
+    def end_headers (self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET')
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
+        SimpleHTTPRequestHandler.end_headers(self)
+    
+
 if __name__ == '__main__':
+    test(CORSRequestHandler, HTTPServer, port=PORT_NUMBER)
     httpd = HTTPServer((HOST_NAME, PORT_NUMBER), Server)
     print(time.asctime(), 'Server UP - %s:%s' % (HOST_NAME, PORT_NUMBER))
     try:
